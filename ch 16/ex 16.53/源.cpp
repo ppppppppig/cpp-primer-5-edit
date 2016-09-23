946693fa4db0d03f3b762d2ed73c283385c9b9cc
ppppppppig <971896266@qq.com>
2016-09-23 14:26:49 +0800

    f

---
+++ ch 16/ex 16.53/源.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <utility>
+#include <string>
+//#include "debug_ret.h"
+using std::cout;
+using std::endl;
+template <typename T>
+void print(std::ostream &, const T &);
+template <typename T,typename ...Args>
+void print(std::ostream &os, const T &t, Args ...args) {
+	os << t << ", ";
+	print(os, args...);
+}
+template <typename T>
+void print(std::ostream &os, const T &t) {
+	os << t;
+}
+int main() {
+	int i = 0; double d = 3.41; std::string s = "fdsfd";
+	struct C {
+		C() = default;
+		int a = 0;
+	}a;
+	print(cout, i, d, s);
+	cout << "gaogaogao" << endl;
+	return 0;
+} 
